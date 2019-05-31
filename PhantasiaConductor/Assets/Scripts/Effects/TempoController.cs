5a4a94098d600edbe6ba3e5353a3c98cd9211e64
bencelsi <hwerbel@uw.edu>
2019-05-31 11:37:20 -0700

    start on tempo controller

---
+++ PhantasiaConductor/Assets/Scripts/Effects/TempoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempoController : MonoBehaviour
+{
+    public AudioSource normal;
+    public float changeDuration = 0.3f;
+    public float fastPitch = 1.2f;
+    public float slowPitch = 0.8f;
+
+    private void Start()
+    {
+    }
+
+    private IEnumerator shiftTempo(float initial, float final)
+    {
+        float counter = 0;
+        while (normal.pitch != final && counter < changeDuration)
+        {
+            counter += Time.deltaTime;
+
+            normal.pitch += Time.deltaTime * (final - initial) / changeDuration;
+            yield return null; ;
+        }
+    }
+}
