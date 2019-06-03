f20bd24481aaab9ca26df5431e68d8c408af3f61
pj <brownasianpj@yahoo.com>
2019-06-03 13:36:01 -0700

    add agnostic hand to scene

---
+++ PhantasiaConductor/Assets/Scripts/CrossPlatform/AgnosticHand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgnosticHand : MonoBehaviour
+{
+    public GameObject steamHand;
+
+    public GameObject oculusHand;
+
+
+    public bool useOculus = false;
+
+    void Awake()
+    {
+        GameObject src = useOculus ? oculusHand : steamHand;
+
+        transform.parent = src.transform;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+    }
+}
