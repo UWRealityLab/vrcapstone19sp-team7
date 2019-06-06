69c340c32890314a1962b3d4de8cc3998ba2d3cd
pj <brownasianpj@yahoo.com>
2019-06-06 15:43:54 -0700

    confetti for gpu boids

---
+++ PhantasiaConductor/Assets/Scripts/Boids/SporadicConfetti.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SporadicConfetti : MonoBehaviour
+{
+    public float confettiChance = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.value < confettiChance) {
+            gameObject.SetActive(true);
+        } else {
+            gameObject.SetActive(false);
+        }
+    }
+}
