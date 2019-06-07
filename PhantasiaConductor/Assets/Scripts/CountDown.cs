26d5246f767d634068e803c7bb74322f19df6562
aj1118 <ajkruse1118@gmail.com>
2019-06-06 17:23:44 -0700

    puzzle skipping UI

---
+++ PhantasiaConductor/Assets/Scripts/CountDown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public int timeLeft = 5;
+    public Canvas darkCanvas;
+    public Text countdown;
+    public GameController controller;
+    void OnEnabled()
+    {
+        Invoke("Dim", 0);
+        countdown.GetComponent<Text>().enabled = true;
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+    }
+    void Update()
+    {
+        countdown.text = ("Skip puzzle in " + timeLeft); 
+        if (timeLeft == 0)
+        {
+            countdown.GetComponent<Text>().enabled = true;
+            controller.SetNextActive();
+            darkCanvas.enabled = false;
+            timeLeft = 5;
+
+        }
+    }
+    
+    IEnumerator LoseTime()
+    {
+        while (this.isActiveAndEnabled)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+
+    }
+
+    void Dim()
+    {
+        darkCanvas.enabled = true;
+    }
+}
---
+++ PhantasiaConductor/Assets/Scripts/SkipUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipUI : MonoBehaviour
+{
+    public float holdTime = 3f;
+    private float holdTimer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            holdTimer += Time.deltaTime;
+
+            if (holdTimer > holdTime)
+            {
+                FindObjectOfType<CountDown>().enabled = true;
+                Invoke("Reset", 5.5f);
+            }
+        }
+    }
+    private void Reset()
+    {
+        FindObjectOfType<CountDown>().enabled = false;
+        holdTimer = 0f;
+    }
+}
