a104954fc6a13028c370d8b79592166b68df8aa8
bencelsi <hwerbel@uw.edu>
2019-06-09 17:06:15 -0700

    try to start star spawning efect

---
+++ PhantasiaConductor/Assets/Scripts/Effects/StarSpawn.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSpawn : MonoBehaviour
+{
+    public GameObject leftHand;
+    public GameObject rightHand;
+    public float spawnInterval = 0.25f;
+    public GameObject prefab;
+
+    private GameObject leftObj;
+    private GameObject rightObj;
+
+    private void OnEnable()
+    {
+        StartCoroutine(RunStarSpawn());
+    }
+
+    private IEnumerator RunStarSpawn()
+    {
+        while (gameObject.activeInHierarchy)
+        {
+            DetachObjects();
+
+            yield return new WaitForSeconds(0.1f);
+
+            leftObj = Instantiate(prefab);
+            Debug.Log(leftObj);
+            rightObj = Instantiate(prefab);
+            Debug.Log(rightObj);
+
+            leftObj.transform.parent = leftHand.transform;
+            leftObj.transform.position = leftHand.transform.position;
+            rightObj.transform.parent = rightHand.transform;
+            rightObj.transform.position = rightObj.transform.position; 
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+        yield return null;
+    }
+
+
+    private void OnDisable()
+    {
+        DetachObjects();
+    }
+
+    private void DetachObjects()
+    {
+        if (leftObj != null)
+        {
+            leftObj.GetComponent<StarSpawnObject>().Detach();
+        }
+        if (rightObj != null)
+        {
+            rightObj.GetComponent<StarSpawnObject>().Detach();
+        }
+    }
+}
---
+++ PhantasiaConductor/Assets/Scripts/Effects/StarSpawnObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSpawnObject : MonoBehaviour
+{
+    public float lifetime = 5f;
+
+    private Vector3 prevPosition;
+    private float cumVelocity;
+    private int sampleCount;
+
+    private void OnEnable()
+    {
+        cumVelocity = 0;
+        prevPosition = transform.position;
+        sampleCount = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        cumVelocity += (transform.position - prevPosition).magnitude / Time.deltaTime;
+        prevPosition = transform.position;
+        sampleCount++;
+    }
+
+    public void Detach()
+    {
+        /*
+        Vector3 newV = transform.forward.normalized;
+        if (transform.parent != null)
+        {
+            newV = transform.parent.transform.forward.normalized;
+        }
+        newV = newV * (cumVelocity / (1.0f * sampleCount));
+        */
+
+        // transform.parent = null;
+        // GetComponent<Rigidbody>().velocity = newV;
+        Invoke("EndOfLife", lifetime);
+    }
+
+    private void EndOfLife()
+    {
+        Destroy(gameObject);
+    }
+}
